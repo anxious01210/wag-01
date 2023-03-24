60f61188a16f76426df7831d61b90a4f3154995f
anxious01210 <pinktheory21@gmail.com>
2023-03-24 12:20:23 +0300

    1st: just some simple changes to home model.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&wwf7jpw28&_@j_qsi(9ui_8_m1g4^139^t-s&g6&pa(5t#v%e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
